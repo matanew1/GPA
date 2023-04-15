4a4a48f56a72e685e337bf92fe7b1aeee3727a22
matanew1 <matan.bardugo@s.afeka.ac.il>
2023-04-15 16:35:46 +0300

    fix data

--- server/app/controllers/transcriptController.js
+++ server/app/controllers/transcriptController.js
@@ -3,8 +3,25 @@ const transcriptService = require("../services/transcriptService")
 // *** Home page 
 exports.loadHomePage = async (req, res) => {
   const { filteredSemesters } = await transcriptService.getTranscript()
-  console.log(filteredSemesters)
   res.json(filteredSemesters);
 };
+
+exports.addData = async (req, res) => {
+  const { year, semester, grades } = req.body;
+
+  // validate data
+  if (!year || !semester || !grades || !Array.isArray(grades)) {
+    return res.status(400).json({ message: 'Invalid data' });
+  }
+
+  try {
+    await transcriptService.addTranscript({ year, semester, grades });
+    res.status(201).json({ message: 'Data added successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Something went wrong' });
+  }
+}
+
 
 
--- server/app/routes/transcriptRoutes.js
+++ server/app/routes/transcriptRoutes.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const transcriptController = require('../controllers/transcriptController');
 
-// api route
 router.get('/', transcriptController.loadHomePage);
+
+router.get('/add-data', (req, res) => {res.render('add-data'); });
+
+router.post('/add-data', transcriptController.addData);
 
 module.exports = router;
